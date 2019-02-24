f0d802c14ce3e0a1a452c3a8683a3a58aa976305
jagh <jagh1729@gmail.com>
2019-02-24 20:42:08 +0100

    Added Grid exploration cell by cell

--- exploration.py
+++ exploration.py
@@ -1,13 +1,89 @@
 ###########################################################
-### Hash Code: Pizza a_example.in                       ###
+### Hash Code 2019                                      ###
+### Pizza Practice Problem                              ###
 ### Team: SuicideSquad                                  ###
 ###########################################################
 
-import sys
 
-def get_pizza(name):
+import sys
+import itertools
+from collections import deque
+
+
+class GridCutter:
+    """
+    Cutting slices from a given grid
+    """
+
+    def __init__(self, grid, first_line):
+        self.grid = grid
+        self.first_line = first_line
+
+        ## setting the input variables
+        row_count, col_count, min_ings, max_area = tuple(map(int, first_line.split(' ')))
+        self.row_count = row_count
+        self.col_count = col_count
+        self.min_ings = min_ings
+        self.max_area = 4 #max_area
+
+        ## Scoring
+        self.slices = deque()
+
+
+    def sliceValidation(self, new_slice):
+        """
+        Check if the slice can be accepted
+        """
+        mroom_count = 0
+        tomato_count = 0
+        cell = 0
+
+        while cell < len(new_slice):
+            if new_slice[cell] == 'M':
+                mroom_count += 1
+            elif new_slice[cell] == 'T':
+                tomato_count += 1
+            cell += 1
+
+        if mroom_count >= self.min_ings and tomato_count >= self.min_ings:
+            self.slices.append(new_slice)
+            print("Slices: {}".format(self.slices))
+
+
+
+    def slicer(self, row, col):
+        """
+        Slice propositon
+        """
+        # print("cell: {},{}".format(row, col))
+        # print("grid: {}".format(self.grid[row]))
+        new_slice = list(itertools.islice(self.grid[row],col, self.max_area))
+        # print("slice: {}".format(new_slice))
+
+        if len(new_slice) >= 2:
+            self.sliceValidation(new_slice)
+
+
+    def explorer(self):
+        """
+        Grid exploration
+        """
+        slices_list = []
+        for row in range(self.row_count):
+            c_beg = 0
+            c_end = 0
+            while c_end < self.col_count:
+                self.slicer(row, c_end)
+                c_end += 1
+
+
+def read_file(filename):
+    """
+    Read the file.
+    Return the first_line and the grid.
+    """
     ## load the file
-    f = open(name, 'r')
+    f = open(filename, 'r')
     first_line = f.readline()
 
     ## setting the input variables
@@ -27,54 +103,67 @@ def get_pizza(name):
     return first_line, grid
 
 
-def right_exploration(first_line, grid):
-    ## setting the input variables
-    row_count, column_count, min_ingredient, max_area = tuple(map(int, first_line.split(' ')))
-    # max_area = 2
+def main():
+    first_line, grid = read_file('dataset/a_example.in') #b_small.in')
+    gc = GridCutter(grid, first_line)
+    gc.explorer()
 
-    ## For each row, I reset the counters
-    results = []
-    slices_list = []
-    ingredients = []
-    for r in range(row_count):
-        beg = 0
-        end = 0
-        mushroom_count = 0
-        tomato_count = 0
 
-        ## scoring cells by slice
-        max_area_count = 0
-        # cell_ben = 0
-        # cell_end = 0
-
-        ## While I'm not at the end of the row,
-        ## I count if I have a tomato or a mushroom
-        while end < column_count:
-            #print("r: {}, c: {}".format(r, end))
-            ## Save the slice_begin
-            if end == 0:
-                cell_ben = (r, end)
-
-            if grid[r][end] == 'M':
-                mushroom_count += 1
-                # print("grip[{},{}]={}".format(r, end, grid[r][end]))
-            elif grid[r][end] == 'T':
-                tomato_count += 1
-
-            ## Check if column is end
-            if end == max_area:
-                #print("r: {}, c: {}".format(r, end))
-                #print("end: {}".format(end))
-                cell_end = (r, end)
-                slices_list.append((cell_ben,cell_end))
-                ingredients.append((tomato_count, mushroom_count))
-
-            end += 1
-
-    print("slices: {}".format(slices_list))
+if __name__ == "__main__":
+    main()
 
 
 
+# def exploration_OLD(first_line, grid):
+    # ## setting the input variables
+    # row_count, column_count, min_ingredient, max_area = tuple(map(int, first_line.split(' ')))
+    # max_area = 1
+    #
+    # ## For each row, I reset the counters
+    # results = []
+    # slices_list = []
+    # ingredients = []
+    #
+    # for r in range(row_count):
+    #     beg = 0
+    #     end = 0
+    #
+    #     mushroom_count = 0
+    #     tomato_count = 0
+    #
+    #     ## scoring cells by slice
+    #     max_area_count = 0
+    #     # cell_ben = 0
+    #     # cell_end = 0
+    #
+    #     ## While I'm not at the end of the row,
+    #     ## I count if I have a tomato or a mushroom
+    #     while end < column_count:
+    #         #print("r: {}, c: {}".format(r, end))
+    #         # print("beg: {}" )
+    #
+    #         ## Save the slice_begin
+    #         if beg == end:
+    #             cell_ben = (r, beg)
+    #
+    #         if grid[r][end] == 'M':
+    #             mushroom_count += 1
+    #             # print("grip[{},{}]={}".format(r, end, grid[r][end]))
+    #         elif grid[r][end] == 'T':
+    #             tomato_count += 1
+    #
+    #         ## Save the silce_end
+    #         if end == max_area:
+    #             #print("r: {}, c: {}".format(r, end))
+    #             #print("end: {}".format(end))
+    #             cell_end = (r, end)
+    #             slices_list.append((cell_ben,cell_end))
+    #             ingredients.append((tomato_count, mushroom_count))
+    #             beg = end+1
+    #
+    #         end += 1
+    #
+    # print("slices: {}".format(slices_list))
 
 # ## While I'm not at the end of the row,
 # ## I count if I have a tomato or a mushroom
@@ -87,7 +176,6 @@ def right_exploration(first_line, grid):
 #         tomato_count += 1
 #     end += 1
 
-
 # # If the slice is too big, I must remove an ingredient
 # def remove_ingredient():
 #     if end - beg > max_area:
@@ -97,11 +185,3 @@ def right_exploration(first_line, grid):
 #         elif grid[r][beg] == 'T':
 #             tomato_count -= 1
 #         beg += 1
-
-def main():
-    first_line, grid = get_pizza('a_example.in')
-    right_exploration(first_line, grid)
-
-
-if __name__ == "__main__":
-    main()
