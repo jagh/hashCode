7cf88da5cee77450b56f55e868bd63cb6c5fe169
jagh <jagh1729@gmail.com>
2019-02-28 20:22:33 +0100

    Added dataSiled objects

--- slideshow.py
+++ slideshow.py
@@ -7,23 +7,38 @@
 
 import sys
 import itertools
-from collections import deque
+import collections
 from collections import OrderedDict
-from sortedcontainers import SortedDict
+
+DataSildes = collections.namedtuple('DataSildes', 'cardinality size tags')
 
 class SlideShow:
     def __init__(self):
         pass
 
 
-
 def read_file(file_path):
     items_corpus = []
     f = open(file_path, 'r')
     for line in f:
-        print(line.split(' '))
-        items_corpus.append(line)
+        content = line.split(' ')
+        # DataSplit(name='cardinality', inputs=X_train, targets=y_train)
+        # DataSildes
+        tags = []
+        count = 0
+        for i in content:
+            if count == 0:
+                card=i
+                count += 1
+                # DataSildes(cardinality=i)
+            elif count ==1:
+                size=i
+                count += 1
+            else:
+                tags.append(i)
+                count += 1
 
+        items_corpus.append(DataSildes(cardinality=card, size=size, tags=tags))
     f.close()
     return items_corpus
 
@@ -31,8 +46,8 @@ def read_file(file_path):
 
 def main():
     file_name='a_example'   #'b_small'   #   #'c_medium'
-    items = read_file('dataset/'+file_name+'.txt')
-    print(items)
+    dataSildes = read_file('dataset/'+file_name+'.txt')
+    print(dataSildes[0].cardinality)
 
 
 if __name__ == "__main__":
