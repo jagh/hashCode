3df7f4b4bf860d30034a14773423c4924278d085
sebastienbx <bonnieux@i3s.unice.fr>
2019-02-28 20:27:19 +0100

    Add class

--- slideshow.py
+++ slideshow.py
@@ -12,9 +12,50 @@ from collections import OrderedDict
 
 DataSildes = collections.namedtuple('DataSildes', 'cardinality size tags')
 
+class Photo:
+    def __init__(self, aligment, nb, tags):
+        self.aligment = aligment
+        self.nb = nb
+        self.tags = tags
+
+    def getTags(self):
+        return self.tags
+
+
+class Slide:
+    def __init__(self, photo):
+        self.photo = photo
+        self.tags
+
+    def intersection(self, toCompare):
+        return list(set(toCompare.getTags()) & set(self.tags))
+
+    def difference(self, toCompare):
+        return list(set(toCompare.getTags()) - set(self.tags))
+
+    def score(self, toCompare):
+        score1 = len(self.intersection(toCompare))
+        score2 = len(self.difference(toCompare))
+        score3 = len(toCompare.difference(self))
+        return min(score1, score2, score3)
+
+
 class SlideShow:
     def __init__(self):
-        pass
+        self.slideshow = list()
+
+    def addPhoto(self, photo):
+        self.slideshow.append(photo)
+
+    def evaluate(self):
+        total_score = 0
+        for i in range(len(self.slideshow)-1):
+            slide1 = self.slideshow[i]
+            slide2 = self.slideshow[i+1]
+            #slide1.intersection(slide2)
+            #slide1.diff(slide2)
+            #slide2.diff(slide1)
+            total_score += slide1.score(slide2)
 
 
 def read_file(file_path):
