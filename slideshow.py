3395b36731f97cc9dfa05794f24e277bddfa1958
jagh <jagh1729@gmail.com>
2019-02-28 20:39:26 +0100

    Updated class

--- slideshow.py
+++ slideshow.py
@@ -1,10 +1,3 @@
-###########################################################
-### Hash Code 2019                                      ###
-### Pizza Practice Problem                              ###
-### Team: SuicideSquad                                  ###
-###########################################################
-
-
 import sys
 import itertools
 import collections
@@ -61,24 +54,25 @@ class SlideShow:
 def read_file(file_path):
     items_corpus = []
     f = open(file_path, 'r')
+    f.readline()
     for line in f:
         content = line.split(' ')
         # DataSplit(name='cardinality', inputs=X_train, targets=y_train)
         # DataSildes
         tags = []
         count = 0
+        print content
         for i in content:
             if count == 0:
                 card=i
                 count += 1
                 # DataSildes(cardinality=i)
-            elif count ==1:
+            elif count == 1:
                 size=i
                 count += 1
             else:
                 tags.append(i)
                 count += 1
-
         items_corpus.append(DataSildes(cardinality=card, size=size, tags=tags))
     f.close()
     return items_corpus
@@ -88,6 +82,7 @@ def read_file(file_path):
 def main():
     file_name='a_example'   #'b_small'   #   #'c_medium'
     dataSildes = read_file('dataset/'+file_name+'.txt')
+    print(dataSildes[0].cardinality)
     print(dataSildes[0].cardinality)
 
 
